09464a67eb5e787cb48c9cafea57c76bd74d0056
Labyrinth-n1 <staradisso@gmail.com>
2024-12-23 12:22:00 +0100

    Installation de Vercel

--- Back/index.js
+++ Back/index.js
@@ -1,35 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const port = 5000;
 
-
-//Middleware
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 let name = '';
 
-
-//Endpoint pour définir le nom
+// Endpoint pour définir le nom
 app.post('/api/name', (req, res) => {
+  const { name: userName } = req.body;
+  name = userName;
+  res.status(201).json({ message: 'Nom enregistré avec succès !!' });
+});
 
-    const {name: userName} = req.body;
-    name = userName;
-    res.status(201).json({message: 'Nom enregistré avec succès !!'})
-})
-
-
-//Endpoint pour récupérer le nom 
-
+// Endpoint pour récupérer le nom
 app.get('/api/name', (req, res) => {
+  res.status(200).json({ name });
+});
 
-    res.status(200).json({name})
-})
-
-app.listen(port, () => {
-    console.log('Ecoute sur le port 5000 !!!')
-})
+// Exporter l'application pour Vercel
+module.exports = app;
